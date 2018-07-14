87beaac7f82e78669988d1b70b378ee3e25e92fb
Ivan Abramov <madrid-beckham@yandex.ru>
2018-07-14 22:41:31 +1000

    Using constructors

--- Lesson2/Jaeger.java
+++ Lesson2/Jaeger.java
@@ -7,61 +7,42 @@ public class Jaeger {
     private int strenght;
     private int armor;
 
-    public int getArmor() {
-        return armor;
-    }
-
-    public void setArmor(int armor) {
-        this.armor = armor;
-    }
-
-    public int getStrenght() {
-        return strenght;
-    }
-
-    public void setStrenght(int strenght) {
+    public Jaeger(String mark, String modelName, String status, double height, double weight, int strenght, int armor) {
+        this.mark = mark;
+        this.modelName = modelName;
+        this.status = status;
+        this.height = height;
+        this.weight = weight;
         this.strenght = strenght;
+        this.armor = armor;
     }
 
     public String getMark() {
         return mark;
     }
 
-    public void setModelName(String modelName) {
-        this.modelName = modelName;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
+    public String getModelName() {
+        return modelName;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public double getHeight() {
+        return height;
     }
 
-    public void setMark(String mark) {
-
-        this.mark = mark;
+    public double getWeight() {
+        return weight;
     }
 
-    public String getModelName() {
-        return modelName;
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public int getArmor() {
+        return armor;
     }
 
     public int attack(int armor, int strenght) {
--- Lesson2/JaegerTest.java
+++ Lesson2/JaegerTest.java
@@ -1,23 +1,12 @@
 public class JaegerTest {
     public static void main(String[] args) {
-        Jaeger jaegerOne = new Jaeger();
-        jaegerOne.setHeight(79.2);
-        jaegerOne.setModelName("Gipsy Danger (Jaeger)");
-        jaegerOne.setArmor(6);
-        jaegerOne.setMark("Mark-3");
-        jaegerOne.setStatus("Destroyed");
-        jaegerOne.setWeight(1.80);
-        jaegerOne.setStrenght(8);
-        System.out.println("Height " + jaegerOne.getHeight());
+        Jaeger jaegerOne = new Jaeger("Mark-3", "Gipsy Danger (Jaeger)", "Destroyed", 79.2,
+                1.80, 8, 6);
+        System.out.println(jaegerOne.getMark());
         System.out.println("Минимальный уровень атаки  " + jaegerOne.attack(jaegerOne.getArmor(), jaegerOne.getStrenght()));
-        Jaeger jaegerTwo = new Jaeger();
-        jaegerTwo.setModelName("StrikerEureka");
-        jaegerTwo.setHeight(76.2);
-        jaegerTwo.setArmor(9);
-        jaegerTwo.setMark("Mark-6");
-        jaegerTwo.setStatus("Destroyed");
-        jaegerTwo.setStrenght(10);
-        jaegerTwo.setWeight(1.850);
+
+        Jaeger jaegerTwo = new Jaeger("Mark-6", "StrikerEureka", "Destroyed", 76.2,
+                1.850, 10, 9);
         System.out.println("Модель " + jaegerTwo.getModelName());
         System.out.println("Минимальный уровень атаки  " + jaegerTwo.attack(jaegerTwo.getArmor(), jaegerTwo.getStrenght()));
     }
