451ba5bbac29b1f6f9922943b962c6d23d249dd6
Ivan Abramov <madrid-beckham@yandex.ru>
2018-08-07 20:22:22 +1000

    инициализируй поля в том порядке, в котором вводил мат. выражение
    1) exampleInput() - что это за название такое? Образец ввода? Этот метод можно разбить еще на два метода: ввод и инициализация
    2) choice переименуй в userAnswer
    3) String choice = sc.nextLine();
               while (!(choice.equals("нет") || choice.equals("да"))) {
                   System.out.print("Вы ввели некорректный ответ. Поробуйте ещё раз [да/нет]: ");
                   choice = sc.nextLine();
               }
    это тоже можно вынести в метод

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,35 +3,45 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
+    private static String mathExpression;
     private static Scanner sc = new Scanner(System.in);
     private static Calculator calculator = new Calculator();
+    private static String userAnswer;
 
     public static void main(String[] args) {
 
         while (true) {
-            exampleInput();
+            input();
+            initialization();
             calculator.consider();
             System.out.print("Хотите продолжить? [да/нет]: ");
-            String choice = sc.nextLine();
-            while (!(choice.equals("нет") || choice.equals("да"))) {
-                System.out.print("Вы ввели некорректный ответ. Поробуйте ещё раз [да/нет]: ");
-                choice = sc.nextLine();
-            }
-            if (choice.equals("нет")) {
+            proceed();
+            if (userAnswer.equals("нет")) {
                 break;
             }
         }
     }
 
-    public static void exampleInput() {
+    public static void input() {
         System.out.print("Введите математическое выражение: ");
-        String mathExpression = sc.nextLine();
+        mathExpression = sc.nextLine();
+    }
+
+    public static void initialization() {
         String[] mathExpress = mathExpression.split(" ");
         calculator.setMathOperation(mathExpress[1]);
         int firstNumber = Integer.parseInt(mathExpress[0]);
         calculator.setFirstNumber(firstNumber);
         int secondNumber = Integer.parseInt(mathExpress[2]);
         calculator.setSecondNumber(secondNumber);
+    }
+
+    public static void proceed() {
+        userAnswer = sc.nextLine();
+        while (!(userAnswer.equals("нет") || userAnswer.equals("да"))) {
+            System.out.print("Вы ввели некорректный ответ. Поробуйте ещё раз [да/нет]: ");
+            userAnswer = sc.nextLine();
+        }
     }
 }
 
