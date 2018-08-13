cfdeeac8426d10ed869500acaeecba611aa9c297
Ivan Abramov <madrid-beckham@yandex.ru>
2018-08-13 22:25:25 +1000

    izm_04_08_18 1) в задании написано "Попробуйте улучшить читаемость кода, разделив его на небольшие методы (рефакторинг)" 2) идея подчеркивает имя переменной mathExpres 3) пиши сеттеры в том порядке, в котором написаны поля 4) инициализируй поля в том порядке, в котором вводил мат. выражение

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -5,22 +5,16 @@ import java.util.Scanner;
 public class CalculatorTest {
     private static Scanner sc = new Scanner(System.in);
     private static Calculator calculator = new Calculator();
-    private static String userAnswer;
 
     public static void main(String[] args) {
+       // String userAnswer;
         do {
             init();
             calculator.consider();
             System.out.print("Хотите продолжить? [да/нет]: ");
-            proceed();
-        } while (!userAnswer.equals("нет"));
+            continueCalculating();
+        } while (!continueCalculating().equals("нет"));
     }
-
-    public static String inputExample() {
-        System.out.print("Введите математическое выражение: ");
-       return sc.nextLine();
-    }
-
     public static void init() {
         String[] mathExpress = inputExample().split(" ");
         int firstNumber = Integer.parseInt(mathExpress[0]);
@@ -29,14 +23,24 @@ public class CalculatorTest {
         int secondNumber = Integer.parseInt(mathExpress[2]);
         calculator.setSecondNumber(secondNumber);
     }
-
-    public static void proceed() {
-        userAnswer = sc.nextLine();
+    public static String continueCalculating() {
+       String userAnswer = sc.nextLine();
         while (!(userAnswer.equals("нет") || userAnswer.equals("да"))) {
             System.out.print("Вы ввели некорректный ответ. Поробуйте ещё раз [да/нет]: ");
-            userAnswer = sc.nextLine();
+            return sc.nextLine();
         }
+        return userAnswer;
     }
+
+    public static String inputExample() {
+        System.out.print("Введите математическое выражение: ");
+       return sc.nextLine();
+    }
+
+
+
+
+
 }
 
 
