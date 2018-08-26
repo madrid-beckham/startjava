4c5845ddde0b51543e6f1ecdc92f73b5bd077979
Ivan Abramov <madrid-beckham@yandex.ru>
2018-08-26 21:47:26 +1000

    метод

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -15,28 +15,30 @@ public class CalculatorTest {
         } while (!answer.equals("нет"));
     }
 
-    public static void init(Calculator calculator) {
+    private static String[] receiveMathExpression() {
+        System.out.print("Введите математическое выражение: ");
+        return sc.nextLine().split(" ");
+    }
+
+    private static void init(Calculator calculator) {
         String[] mathExpress = receiveMathExpression();
         calculator.setFirstNumber(Integer.parseInt(mathExpress[0]));
         calculator.setMathOperation(mathExpress[1]);
         calculator.setSecondNumber(Integer.parseInt(mathExpress[2]));
     }
 
-    public static String[] receiveMathExpression() {
-        System.out.print("Введите математическое выражение: ");
-        return sc.nextLine().split(" ");
-    }
-
-    public static String getUserAnswer() {
+    private static String getUserAnswer() {
         System.out.print("Хотите продолжить? [да/нет]: ");
         return sc.nextLine();
     }
 
-    public static String testUserAnswer() {
+    private static String testUserAnswer() {
         String userAnswer;
         do {
             userAnswer = getUserAnswer();
         } while (!(userAnswer.equals("нет") || userAnswer.equals("да")));
         return userAnswer;
     }
+
+
 }
